7d7831cf023e227d1bb7353db8c995c6639fed98
jey707 <jey707@naver.com>
2022-06-16 19:45:15 +0900

    side-bar update

--- js/common.js
+++ js/common.js
@@ -40,7 +40,8 @@ document.addEventListener("click", function (e) {
       if (
         !target.classList.contains("show-menu") &&
         !target.classList.contains("list-li") &&
-        !target.classList.contains("mhd-more")
+        !target.classList.contains("mhd-more") &&
+        !target.classList.contains("arr-right")
       ) {
         document.querySelector(".mhd").classList.remove("show-menu");
 
@@ -51,3 +52,22 @@ document.addEventListener("click", function (e) {
   }
 });
 menuBtn.addEventListener("click", toggleMenu);
+
+// side menubar controllor
+$(document).ready(function () {
+  $(".mhd ul .mhd-sub").hide();
+  $(".mhd-more").click(function () {
+    let slideup = document.querySelector(".mhd").querySelectorAll(".mhd-sub");
+    let arrowUp = document
+      .querySelector(".mhd")
+      .querySelectorAll(".open-arrow");
+    $(slideup).each(function (i, obj) {
+      $(slideup[i]).slideUp("fast");
+    });
+    $(arrowUp).each(function (i, obj) {
+      $(arrowUp[i]).removeClass("open-arrow");
+    });
+    $(this.parentElement.querySelector(".mhd-sub")).slideToggle("fast");
+    $(this).addClass("open-arrow");
+  });
+});
